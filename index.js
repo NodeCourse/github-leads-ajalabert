74332eb0cce164dd4727633e95ccf1f0593b6e19
Adrien Jalabert <35221770+ajalabert@users.noreply.github.com>
2018-06-08 13:13:17 +0200

    add save to csv + first search repos

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
 const program = require('commander');
-const octokit = require('@octokit/rest');
+const octokit = require('@octokit/rest')();
+const stringify = require('csv-stringify');
+const fs = require('fs');
 
 program
   .version('0.1.0')
-  .option('-p, --peppers', 'Add peppers')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .option('-t, --token [token]', 'Specify your github [token]')
   .parse(process.argv);
 
-
-if (program.peppers){
-    console.log('peppers');
-}
-    
-
-if (program.cheese){
-    console.log('cheese');
-}
-
-if(process.token){
+if (program.token){
     octokit.authenticate({
-        token: 'token',
+        type: 'token',
         token: program.token
     });
+
+    octokit.search.repos({
+        q: 'language:c#'
+    })
+    .then(result => {
+        return result.data.items;
+    }).then(items =>{
+        saveToFile(items);
+        console.log(items[0].name);
+    })
+
+    // octokit.repos.getForOrg({
+    //     org: 'octokit',
+    //     type: 'public'
+    //   }).then(({data, headers, status}) => {
+    //     console.log("data= " + data);
+    //   })
+    .catch((error => {
+        console.log(error);
+    }));
 }
+
+function saveToFile(items){
+    let data = [];
+    let columns = {
+    id: 'id',
+    name: 'Name'
+    };
+
+    for (var i = 0; i < 10; i++) {
+    data.push([i, 'Name ' + items[i].name]);
+    }
+
+    stringify(data, { header: true, columns: columns }, (err, output) => {
+        if (err) throw err;
+        fs.writeFile('my.csv', output, (err) => {
+            if (err) throw err;
+            console.log('my.csv saved.');
+        });
+    });
+}
+
+
+
+// if(process.token){
+//     octokit.authenticate({
+//         token: 'token',
+//         token: program.token
+//     });
+// }
